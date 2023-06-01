5381124ee47e384b58a78832f7ff10bf47e20842
Murilo Pontes <murilocurypontes@gmail.com>
2023-06-01 11:06:36 -0300

    adding new project and fixing indentations

--- labex/trabnofunc.c
+++ labex/trabnofunc.c
@@ -1,60 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int conditiontest(char posicoes[3][3], int j, int k){
-    if(posicoes[j][0] == posicoes[j][1] && posicoes[j][0] == posicoes [j][2])
-        return 1;
-        
-    if(posicoes[0][k] == posicoes[1][k] && posicoes[0][k] == posicoes [2][k])
-        return 1;
-    
-    if(j!=1 && k!=1 && k!=j)//fowardslash
-        if(posicoes[0][2] == posicoes[1][1] && posicoes[0][2] == posicoes [2][0])
-        return 1;
-            
-    if(j!=1 && k!=1 && k==j)//backslash
-        if(posicoes[0][0] == posicoes[1][1] && posicoes[0][0] == posicoes [2][2])
-            return 1;
-        
-        if(j==1 && k==1){  //ambas diagonais
-            if(posicoes[0][2] == posicoes[1][1] && posicoes[0][2] == posicoes [2][0])
-                return 1;
-                
-            if(posicoes[0][0] == posicoes[1][1] && posicoes[0][0] == posicoes [2][2])
-                return 1;
-            
-        }
-}
-
-void tabuleiro(char posicoes[3][3]){
-    int i, j;
-    printf("\n");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            if(posicoes [i][j] == 'e'){
-                if(j==0 || j==1) 
-                    printf("   │");   
-                else
-                    printf("   \n");
-            }
-            else{
-                if(j==0 || j==1) 
-                    printf(" %c │", posicoes[i][j]);   
-                else
-                    printf(" %c \n", posicoes[i][j]);
-            }
-        }
-        if (i<2)
-            printf("───┼───┼───\n");
-    }
-    printf("\n");
-}
-
 int main(){
     char posicoes[3][3];
     char simb, sgameon[3], sj[3],sk[3];
     int gameon = 1, vitoria = 0;
-    int i, j, k;
+    int i, j, k, l, m;
     int escolha_j, escolha_k;
 
     while (gameon){
@@ -84,7 +35,7 @@ int main(){
             }
                 printf("Erro -> Símbolo incorreto\n");
         }
-
+        //inicializa a matriz com 'e' em todas as posicoes
         for(i=0;i<3;i++){
             for (j=0;j<3;j++){
                 posicoes[i][j] = 'e';
@@ -116,8 +67,26 @@ int main(){
             else {
                 posicoes[j][k] = simb;
                 system("clear");
-
-                tabuleiro(posicoes);
+                //printa o tabuleiro na tela
+                for(l=0;l<3;l++){
+                    for(m=0;m<3;m++){
+                        if(posicoes [l][m] == 'e'){
+                            if(m==0 || m==1) 
+                                printf("   │");   
+                            else
+                                printf("   \n");
+                        }
+                        else{
+                            if(m==0 || m==1) 
+                                printf(" %c │", posicoes[l][m]);   
+                            else
+                                printf(" %c \n", posicoes[l][m]);
+                        }
+                    }
+                    if (l<2)
+                        printf("───┼───┼───\n");
+                }
+                printf("\n");
 
                 //antes da quinta jogada, n tem como ganhar
                 if(i < 5) {
@@ -128,7 +97,28 @@ int main(){
                     continue;
                 }
 
-                vitoria = conditiontest(posicoes, j, k);
+                //teste de condições de vitória
+                if(posicoes[j][0] == posicoes[j][1] && posicoes[j][0] == posicoes [j][2])
+                    vitoria = 1;
+        
+                if(posicoes[0][k] == posicoes[1][k] && posicoes[0][k] == posicoes [2][k])
+                    vitoria = 1;
+    
+                if(j!=1 && k!=1 && k!=j)//fowardslash
+                    if(posicoes[0][2] == posicoes[1][1] && posicoes[0][2] == posicoes [2][0])
+                        vitoria = 1;
+            
+                if(j!=1 && k!=1 && k==j)//backslash
+                    if(posicoes[0][0] == posicoes[1][1] && posicoes[0][0] == posicoes [2][2])
+                        vitoria = 1;
+        
+                if(j==1 && k==1){  //ambas diagonais
+                    if(posicoes[0][2] == posicoes[1][1] && posicoes[0][2] == posicoes [2][0])
+                        vitoria = 1;
+                
+                    if(posicoes[0][0] == posicoes[1][1] && posicoes[0][0] == posicoes [2][2])
+                        vitoria = 1;
+                }
                 
                 if(vitoria == 1)
                     break;
